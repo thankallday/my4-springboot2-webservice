a89e0da723b2d90088d4631182b017f630ed37f2
ex <ex@com>
2020-02-23 18:05:01 -0500

    보기 좋게 edited & commented

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
                     // p92 Entity 클래스에서는 절대 Setter 메소드를 만들지 않는다. 대신 해당 필드의 값 변경이 필요하면 명확히 그 목적과 의도를 나타낼 수 있는 메소드를 추가해야 한다
 @NoArgsConstructor  // 5 lombok 어노테이션. lombok 이 기본 생성자 자동추가.
 @Entity             // 1 JPA 어노테이션. 테이블과 링크될 클래스임을 나타낸다. 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭한다. SalesManager.java --> sales_manager table
-public class Posts {  // 실제 DB 테이블과 매칭될 클래스이며 보통 Entity 클래스 라고도 한다. DB 데이터에 작업할 경우 실제 퀴리를 날리기 보다, 이 Entity 클래스의 수정을 통해 작업한다
+public class Posts {// 실제 DB 테이블과 매칭될 클래스이며 보통 Entity 클래스 라고도 한다. DB 데이터에 작업할 경우 실제 퀴리를 날리기 보다, 이 Entity 클래스의 수정을 통해 작업한다
 
   @Id   // 2 해당 테이블의 PK 필드
   @GeneratedValue(strategy = GenerationType.IDENTITY) // 3 PK 의 생성규칙을 나타낸다. GenerationType.IDENTITY 옵션을 추가해야 auto-increment 된다.
@@ -26,6 +26,7 @@ public class Posts {  // 실제 DB 테이블과 매칭될 클래스이며 보통
 
   private  String author;
 
+  // 입력용
   @Builder  // 7 lombok 이 해당 클래스의 빌더 패턴 클래스를 생성. 생성자 상단에 선언시 생성자에 포함된 필드만 빌더에 포함
             //          p93 이 책에서는 생성자 대신에 @Builder 를 통해 제공되는 빌더 클래스를 사용한다. 어느 필드에 어떤 값을 채워야 할지 명확히 하기 위해.
   public Posts(String title, String content, String author) {
@@ -34,7 +35,7 @@ public class Posts {  // 실제 DB 테이블과 매칭될 클래스이며 보통
     this.author = author;
   }
 
-  // p112 수정 용
+  // p112 수정용
   public void update (String title, String content) {
     this.title = title;
     this.content = content;
