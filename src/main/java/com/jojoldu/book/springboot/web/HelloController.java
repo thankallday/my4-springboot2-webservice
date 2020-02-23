9b191201967dd514243cc8148d412ae5186fee9d
ex <ex@com>
2020-02-23 17:42:51 -0500

    보기 좋게 edited & commented

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -10,16 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController             // 1 컨트롤러를 JSON 을 반환하는 컨트롤러로 만들어 준다. 예전엔 @RequestBody
 public class HelloController {
 
+  // http://localhost:8080/hello
   @GetMapping("/hello")   // 2 HTTP 메소드인 Get 의 요청을 받을수 있는 API 를 만들어 준다. 예전엔 @RequestMapping
   public String hello() {
     return "hello";
   }
 
   // p75
+  // http://localhost:8080/hello/dto?name=aaa&amount=999
   @GetMapping("/hello/dto")
   public HelloResponseDto helloDto(@RequestParam("name") String name, // 1 @RequestParam 외부에서 API 로 넘긴 파라미터를 가져오는 어노테이션
                                    @RequestParam("amount") int amount) {
-
     return new HelloResponseDto(name, amount);
   }
 }
--- src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
@@ -5,7 +5,7 @@ package com.jojoldu.book.springboot.web.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@Getter     // 1 선언된 모든 필드의 get 메소드를 생성해 준다
+@Getter                     // 1 선언된 모든 필드의 get 메소드를 생성해 준다
 @RequiredArgsConstructor    // 2 선언된 모든 final 필드가 포함된 생성자를 생성해 준다. final 이 없는 필드는 생성자에 포함되니 않는다
 public class HelloResponseDto {
   private final String name;
