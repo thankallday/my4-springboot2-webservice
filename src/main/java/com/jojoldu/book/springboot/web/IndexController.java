856e2fdbfd064aa40561b1e81a88271d0d8f1010
ex <ex@com>
2020-02-24 19:56:31 -0500

    4.3 게시글 등록 화면 만들기 (by p145)

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -2,15 +2,23 @@ package com.jojoldu.book.springboot.web;
 
 // p132 mustache 로 화면 구성하기 - 기본 페이지 만들기
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
   @GetMapping("/")
   public String index() {
 
-    return "index";
+    return "index";      // call index.mustache
+  }
+
+  // 138
+  @GetMapping("/posts/save")
+  public String postsSave() {
+    return "posts-save"; // call posts-save.mustache
   }
 }
